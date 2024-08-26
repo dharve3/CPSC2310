aefb30332a00546201b1affa946d1b98d5557853
D-Guy Laptop <ddcool535@gmail.com>
2024-08-26 09:55:10 -0400

    Updated bitfield examples

--- lecture/pointerReview/structReview/structWBitFieldBroken.c
+++ lecture/pointerReview/structReview/structWBitFieldBroken.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 struct date {
 	int d : 5;
-	int m :4;
+	int m : 4;
 	int y; 
 }; 
 
 int main() 
 { 
+	// integer overflow
 	struct date dt = { 31, 12, 2020 }; 
 	printf("Date is %d/%d/%d\n", dt.d, dt.m, dt.y); 
 	printf("%lu", sizeof(struct date));
