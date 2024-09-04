7e40194b376aab1e76a72a0eaef7d8f342aac066
D-Guy Laptop <ddcool535@gmail.com>
2024-09-04 10:03:05 -0400

    Added comments

--- lecture/structAndPointerReview/ptr_ex26.c
+++ lecture/structAndPointerReview/ptr_ex26.c
@@ -5,15 +5,15 @@ int main()
 {
   int i = 6, j = 2;
   f(&i, j);
-  printf("i = %d j = %d\n", i, j);
+  printf("i = %d j = %d\n", i, j); // i = 16, j = 2
 
-  printf("%d\n", i + j);
+  printf("%d\n", i + j); // 18
 
   return 0;
 }
 
 void f(int* p, int m)
 {
-  m = m + 8;
-  *p = *p + m;
+  m = m + 8; // 10 (value)
+  *p = *p + m; // 16 (reference)
 }
